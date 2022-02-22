855a6c2578fa672bd94d6229390e40753319e3cf
Srishti Singhal <99279286+Srishti-Singhal@users.noreply.github.com>
2022-02-22 17:55:00 +0530

    Add files via upload

---
+++ Gigasecond.py
@@ -0,0 +1,6 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import datetime
+
+def add(moment):
+    return moment + datetime.timedelta(seconds=1000000000)
